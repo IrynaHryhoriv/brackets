107f96b75e7b02996f51c435a93268f9b1747917
Your NameIryna <irina.hr@gmail.com>
2023-02-21 20:48:02 -0800

    add: code to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let countStr = str;
+  let i = 0;
+
+  function cleansDouble (){
+    bracketsConfig.forEach(function(item){
+    countStr = countStr.replace(`${item[0]}${item[1]}`, '');
+    }
+    )};
+        
+    do{
+    cleansDouble (); i++; } while (i < 50);
+       
+     let result = (countStr.length === 0) ? true : false;
+        
+    return result;
 }
